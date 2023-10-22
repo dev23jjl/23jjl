1aca025ccc60a468b371b3cb62a179912534d3f3
James <james.johanningmeier@gmail.com>
2023-10-22 01:01:14 +0000

    add umami analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/alpine',
-
+  extends: ['@nuxt-themes/alpine', 'nuxt-umami'],
+  appConfig: {
+    umami: {
+      host: 'https://analytics.eu.umami.is',
+      id: '41648874-8908-4e0b-ae5b-bc5df5f02d05',
+      version: 2,
+      debug: true,
+    },
+  },
   modules: [
-    '@nuxtjs/plausible',
     '@nuxt/devtools'
-  ]
+  ],
 })
